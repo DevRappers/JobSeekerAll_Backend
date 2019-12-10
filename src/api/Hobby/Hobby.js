729a74402e0feb3244b8ea8b999bd9d5feb299f3
DevRappers <devrappers@gmail.com>
2019-12-10 16:56:46 +0900

    postsCount수정

--- src/api/Hobby/Hobby.js
+++ src/api/Hobby/Hobby.js
@@ -3,7 +3,7 @@ import { prisma } from '../../../generated/prisma-client';
 export default {
 	Hobby: {
 		posts: ({ id }) => prisma.hobby({ id }).posts(),
-		postsCount: ({ id }) => prisma.postsConnection({ where: { user: { id } } }).aggregate().count(),
+		postsCount: ({ id }) => prisma.postsConnection({ where: { hobby: { id } } }).aggregate().count(),
 		user: ({ id }) => prisma.hobby({ id }).user()
 	}
 };
