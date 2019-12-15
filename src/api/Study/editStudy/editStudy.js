ef9bb2ded732ceb45ca239e5dc2d6a130cac9ef0
DevRappers <devrappers@gmail.com>
2019-12-15 14:49:59 +0900

    스터디 수정 Actions 제거

--- src/api/Study/editStudy/editStudy.js
+++ src/api/Study/editStudy/editStudy.js
@@ -7,22 +7,20 @@ export default {
 	Mutation: {
 		editStudy: async (_, args, { request, isAuthenticated }) => {
 			isAuthenticated(request);
-			const { id, title, caption, information, job, area, time, action } = args;
+			const { id, title, caption, information, job, area, time } = args;
 			const { user } = request;
 
 			const study = await prisma.$exists.study({ id, user: { id: user.id } });
 
 			if (study) {
-				if (action === EDIT) {
-					return prisma.updateStudy({
-						data: { title, caption, information, job, area, time },
-						where: { id }
-					});
-				} else if (action === DELETE) {
-					return prisma.deleteStudy({ id });
-				}
+				await prisma.updateStudy({
+					data: { title, caption, information, job, area, time },
+					where: { id }
+				});
+				return true;
 			} else {
 				throw Error('오류발생! 해당하는 스터디가 없습니다.');
+				return false;
 			}
 		}
 	}
