f65b61f8ed96f280d2d1864f85d9aa8dec461c0f
DevRappers <devrappers@gmail.com>
2019-12-17 11:32:25 +0900

    댓글 달기 수정

--- src/api/Comment/addComment/addComment.js
+++ src/api/Comment/addComment/addComment.js
@@ -4,22 +4,27 @@ export default {
 	Mutation: {
 		addComment: async (_, args, { request, isAuthenticated }) => {
 			isAuthenticated(request);
-			const { text, postId } = args;
+			const { text, hobbyId } = args;
 			const { user } = request;
-			const comment = await prisma.createComment({
-				user: {
-					connect: {
-						id: user.id
-					}
-				},
-				post: {
-					connect: {
-						id: postId
-					}
-				},
-				text
-			});
-			return comment;
+			try {
+				await prisma.createComment({
+					user: {
+						connect: {
+							id: user.id
+						}
+					},
+					hobby: {
+						connect: {
+							id: hobbyId
+						}
+					},
+					text
+				});
+				return true;
+			} catch (e) {
+				console.log(e);
+				return false;
+			}
 		}
 	}
 };
