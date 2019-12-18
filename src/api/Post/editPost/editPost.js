aa329c1dde7feb1d862a5d8e482f9e2af55de92d
DevRappers <devrappers@gmail.com>
2019-12-18 16:59:03 +0900

    포스트 수정 백앤드 수정

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -1,25 +1,29 @@
 import { prisma } from '../../../../generated/prisma-client';
 
-const DELETE = 'DELETE';
-const EDIT = 'EDIT';
-
 export default {
 	Mutation: {
 		editPost: async (_, args, { request, isAuthenticated }) => {
 			isAuthenticated(request);
-			const { id, caption, title, action } = args;
+			const { id, caption, title, file } = args;
 			const { user } = request;
 			const post = await prisma.$exists.post({ id, user: { id: user.id } });
 			if (post) {
-				if (action === EDIT) {
-					return prisma.updatePost({
-						data: { caption, title },
-						where: { id }
-					});
-				} else if (action === DELETE) {
-					return prisma.deletePost({ id });
-				}
+				await prisma.updatePost({
+					data: { caption, title },
+					where: { id }
+				});
+
+				await prisma.updateFile({
+					data: { url: file },
+					where: {
+						post: {
+							id
+						}
+					}
+				});
+				return true;
 			} else {
+				return false;
 				throw Error('삭제실패');
 			}
 		}
